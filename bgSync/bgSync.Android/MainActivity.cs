d53aa1134f625cf9e23300b4ac0407eca08559ad
Andre <andre@playmoresoftware.com>
2018-06-20 20:41:25 -0300

    jobScheduler implemented

--- bgSync/bgSync.Android/MainActivity.cs
+++ bgSync/bgSync.Android/MainActivity.cs
@@ -1,17 +1,17 @@
-﻿using System;
-
-using Android.App;
+﻿using Android.App;
+using Android.App.Job;
 using Android.Content.PM;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
+using bgSync.Droid.Helpers;
+using Xamarin.Forms.Platform.Android;
 
 namespace bgSync.Droid
 {
-    [Activity(Label = "bgSync", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    [Activity(Label = "bgSync", Icon = "@mipmap/icon", Theme = "@style/MainTheme",
+        MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : FormsAppCompatActivity
     {
+
         protected override void OnCreate(Bundle bundle)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
@@ -19,9 +19,42 @@ namespace bgSync.Droid
 
             base.OnCreate(bundle);
 
-            global::Xamarin.Forms.Forms.Init(this, bundle);
+            Xamarin.Forms.Forms.Init(this, bundle);
+
             LoadApplication(new App());
+
+            var jobBuilder = this.CreateJobBuilderUsingJobId<Services.ScheduledService>(1);
+            var jobInfo = jobBuilder
+                .SetMinimumLatency(1000)   // Wait at least 1 second
+                .SetOverrideDeadline(5000) // But no longer than 5 seconds
+                .SetPersisted(true) // Allow it to keep running after a device restart
+                .SetRequiredNetworkType(NetworkType.NotRoaming) //basic necessary network connection to run the job
+                .Build();
+
+            var jobScheduler = (JobScheduler)GetSystemService(JobSchedulerService);
+            jobScheduler.Schedule(jobInfo);
         }
+
+        protected override void OnPause()
+        {
+            base.OnPause();
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+        }
+
+        protected override void OnStart()
+        {
+            base.OnStart();
+        }
+
+        protected override void OnStop()
+        {
+            base.OnStop();
+        }
+        
     }
 }
 
--- bgSync/bgSync/MainPage.xaml.cs
+++ bgSync/bgSync/MainPage.xaml.cs
@@ -9,9 +9,28 @@ namespace bgSync
 {
 	public partial class MainPage : ContentPage
 	{
-		public MainPage()
-		{
+        Label output;
+        public MainPage()
+        {
 			InitializeComponent();
-		}
+            
+        }
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            MessagingCenter.Subscribe<MainPage>(this, "Hi", (sender) => {
+                Device.BeginInvokeOnMainThread(() => {
+                    lbTeste.Text = "Received 'hello' at " + DateTime.Now;
+
+                    var sync = new DoSyncJob();
+                    sync.Start();
+                });
+            });
+        }
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            MessagingCenter.Unsubscribe<MainPage>(this, "Hi");
+        }
 	}
 }
