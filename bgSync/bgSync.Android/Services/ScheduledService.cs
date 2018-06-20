29bed7fccf3d017598c11887a686685df7d1f161
Andre <andre@playmoresoftware.com>
2018-06-20 20:41:52 -0300

    jobScheduler implemented

---
+++ bgSync/bgSync.Android/Services/ScheduledService.cs
@@ -0,0 +1,39 @@
+﻿using Android.App;
+using Android.App.Job;
+using System;
+using System.Threading.Tasks;
+
+namespace bgSync.Droid.Services
+{
+    [Service(Name = "com.biosev.autoatendimento.controleAcesso.ControleAcessoSyncScheduledJob",
+         Permission = "android.permission.BIND_JOB_SERVICE")]
+    public class ScheduledService : JobService
+    {
+
+        public override bool OnStartJob(JobParameters jobParams)
+        {
+            Task.Run(() =>
+            {
+                Xamarin.Forms.Device.StartTimer(
+                    new TimeSpan(0, 0, 5), //post a message every 5 secs
+                    () =>
+                    {
+                        Xamarin.Forms.MessagingCenter.Send<MainPage>((MainPage)App.Current.MainPage, "Hi");
+                        return true;
+                    });
+
+                //Uncomment the following line if you want to terminate the job
+                //JobFinished(jobParams, false);
+            });
+
+            // Return true because of the asynchronous work
+            return true;
+        }
+
+
+        public override bool OnStopJob(JobParameters jobParams)
+        {
+            return false;
+        }
+    }
+}
---
+++ bgSync/bgSync/DoSyncJob.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bgSync
+{
+    public class DoSyncJob
+    {
+        public async void Start()
+        {
+            await Task.Run(() =>
+            {
+                // do work
+            });
+        }
+    }
+}
